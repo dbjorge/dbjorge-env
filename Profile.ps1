81a8b09b536ccedddeaeb404414414bb728bb203
Dan Bjorge <dan@dbjorge.net>
2016-08-20 12:48:47 -0700

    Add chromium depot_tools to PATH

--- Profile.ps1
+++ Profile.ps1
@@ -6,6 +6,14 @@ function PathStringWith ($OriginalPathString, $Term) {
     }
 }
 
+$DesiredPathElements = @(
+    'C:\Code\depot_tools'
+)
+
+foreach ($Element in $DesiredPathElements) {
+    $env:PATH = PathStringWith $env:PATH $Element
+}
+
 New-Alias -Name 'which' -Value 'Get-Command'
 New-Alias -Name 'subl' -Value 'C:\Program Files\Sublime Text 3\subl.exe'
 New-Alias -Name 'code' -Value 'C:\Program Files (x86)\Microsoft VS Code\Code.exe'
