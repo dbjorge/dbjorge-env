b0ecfe4ef06c6c75e43a193b429087655f49fb8e
Dan Bjorge <dan@dbjorge.net>
2016-08-21 11:11:29 -0700

    Improve output of RunPester.ps1

---
+++ RunPester.ps1
@@ -0,0 +1,31 @@
+# From https://github.com/stuartleeks/posh-dnvm/blob/master/RunPester.ps1
+
+function GetLineNumber($stackTrace){
+	if($stackTrace -match "at line: (\d*)"){
+		$matches[1];
+	} else {
+		$null
+	}
+}
+function GetFileName($stackTrace){
+	if($stackTrace -match "at line: (?:\d*) in (.*)\n"){
+		$matches[1];
+	} else {
+		$null
+	}	
+}
+function FormatResult ($result){
+	process {
+		if ($null -eq $_ -or $null -eq $_.StackTrace) { return }
+
+		$lineNumber = GetLineNumber $_.StackTrace
+		$file = GetFileName $_.StackTrace | Resolve-Path -Relative
+		$collapsedMessage = $_.FailureMessage -replace "`n"," "
+		$testDescription = "$($_.Describe):$($_.Name)"
+		"$file;$lineNumber;${testDescription}:$collapsedMessage"
+	}
+}
+Write-Output "Running tests..."
+$results = Invoke-Pester -PassThru -PesterOption (New-PesterOption -IncludeVSCodeMarker)
+$results.TestResult | Where-Object { -not $_.Passed} | FormatResult
+Write-Output "Done"
