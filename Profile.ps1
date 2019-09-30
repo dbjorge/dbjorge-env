e7eb70e1486829a046c29873825f88aa1e7cb6df
Dan Bjorge <dan@dbjorge.net>
2019-09-29 17:00:57 -0700

    remove obsolete profile bits

--- Profile.ps1
+++ Profile.ps1
@@ -1,24 +1,18 @@
 Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
 
-$EnvDirectory = $PSScriptRoot # Typically C:\Code\dbjorge-env
-$CodeDirectory = Split-Path $PSScriptRoot #Typically C:\Code
+Import-Module posh-git
+
+$EnvDirectory = $PSScriptRoot
 $EnvPSModuleDirectory = Join-Path $EnvDirectory 'PSModules'
 Import-Module -Force "$EnvPSModuleDirectory\Paths\Paths.psm1"
-
-Register-PathElement -PathElement @(
-    (Join-Path $CodeDirectory 'depot_tools')
-)
 
 Register-PathElement -PathEnvironmentVariable 'PSModulePath' -PathElement @(
     $EnvPSModuleDirectory
 )
 
-$env:DEPOT_TOOLS_WIN_TOOLCHAIN = 0
-
-New-Alias -Force -Name 'subl' -Value 'C:\Program Files\Sublime Text 3\subl.exe'
-New-Alias -Force -Name 'which' -Value 'Get-Command'
-
 foreach ($ModuleDirectory in (Get-ChildItem $EnvPSModuleDirectory)) {
     Import-Module -Force -Name $ModuleDirectory.Name
 }
+
+New-Alias -Force -Name 'which' -Value 'Get-Command'
